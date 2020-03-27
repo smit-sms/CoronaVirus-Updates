94cba3badf6bf68d0a2e3cf4f2808cc96a35bcb8
smit-sms <smit16@somaiya.edu>
2020-03-27 15:47:45 +0530

    Updated Code for Indian Data

--- CoronaVirus.py
+++ CoronaVirus.py
@@ -1,42 +1,58 @@
-from selenium import webdriver
-from time import sleep
-import re
-from datetime import datetime
-import smtplib
 
-class CoronaVirus():
-    def __init__(self):
-        self.driver = webdriver.Chrome()
+# importing libraries 
+import Tkinter
+import requests 
+from bs4 import BeautifulSoup 
+from tabulate import tabulate 
+import os 
+import numpy as np 
+import matplotlib.pyplot as plt 
+extract_contents = lambda row: [x.text.replace('\n', '') for x in row] 
+URL = 'https://www.mohfw.gov.in/'
+  
+SHORT_HEADERS = ['SNo', 'State','Indian-Confirmed', 
+                 'Foreign-Confirmed','Cured','Death'] 
+  
+response = requests.get(URL).content 
+soup = BeautifulSoup(response, 'html.parser') 
+header = extract_contents(soup.tr.find_all('th')) 
+  
+stats = [] 
+all_rows = soup.find_all('tr') 
+  
+for row in all_rows: 
+    stat = extract_contents(row.find_all('td')) 
+    if stat: 
+        if len(stat) == 5: 
+            # last row 
+            stat = ['', stat] 
+            stats.append(stat) 
+        elif len(stat) == 6: 
+            stats.append(stat) 
+  
+stats[-1][1] = "Total Cases"
+  
+stats.remove(stats[-1]) 
 
-    def get_data(self):
-        print("start")
-        # try:
-        self.driver.get('https://worldometers.info/coronavirus/')
-        table = self.driver.find_element_by_xpath('//*[@id="main_table_countries"]/tbody[1]')
-        country_element = table.find_element_by_xpath("//td[contains(text(), 'China')]")
-        row = country_element.find_element_by_xpath("./..")
-        data = row.text.split(" ")
-        total_cases = data[1]
-        new_cases = data[2]
-        total_deaths = data[3]
-        new_deaths = data[4]
-        active_cases = data[5]
-        total_recovered = data[6]
-        serious_critical = data[7]
+objects = [] 
+for row in stats : 
+    objects.append(row[1])  
+  
+y_pos = np.arange(len(objects)) 
+  
+performance = [] 
+for row in stats : 
+    performance.append(int(row[2]) + int(row[3])) 
+  
+table = tabulate(stats, headers=SHORT_HEADERS) 
+print(table) 
 
-        print("Country: " + country_element.text)
-        print("Total cases: " + total_cases)
-        print("New cases: " + new_cases)
-        print("Total deaths: " + total_deaths)
-        print("New deaths: " + new_deaths)
-        print("Active cases: " + active_cases)
-        print("Total recovered: " + total_recovered)
-        print("Serious, critical cases: " + serious_critical)
-
-        self.driver.close()
-        # except:
-            # print("ended")
-            # self.driver.quit()  
-
-bot = CoronaVirus()
-bot.get_data()              
+plt.barh(y_pos, performance, align='center', alpha=0.5, 
+                 color=(234/256.0, 128/256.0, 252/256.0), 
+                 edgecolor=(106/256.0, 27/256.0, 154/256.0)) 
+  
+plt.yticks(y_pos, objects) 
+plt.xlim(1,80) 
+plt.xlabel('Number of Cases') 
+plt.title('Corona Virus Cases') 
+plt.show() 
